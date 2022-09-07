8e0df74d7d773dedceea94b0e652f3ac9cdac567
Hamilton Resende <hamilton.rferreira@gmail.com>
2022-09-07 14:43:35 -0300

    Iniciando projeto utilizando spring boot, spring security, hybernate e H2 database.

---
+++ src/main/java/com/socialweb/socialweb/SocialwebApplication.java
@@ -0,0 +1,13 @@
+package com.socialweb.socialweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialweb/socialweb/SocialwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialweb.socialweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
