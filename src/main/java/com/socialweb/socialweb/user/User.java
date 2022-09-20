51332422eacb6ea296455649ab1f2a22233ea06e
Hamilton Resende <hamilton.rferreira@gmail.com>
2022-09-20 18:27:38 -0300

    Criando método para salvar usuário no banco. Criando método de teste para salvar usuário no banco.

--- src/main/java/com/socialweb/socialweb/user/User.java
+++ src/main/java/com/socialweb/socialweb/user/User.java
@@ -2,9 +2,22 @@ package com.socialweb.socialweb.user;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 // O lombok através do @Data implementa os métodos getters and setters, construtores e mais por default
 @Data
+@Entity
+// A anotacao @Table possibilita setar o nome customizado para a tabela users
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
     private String userName;
 
     private String displayName;
--- src/test/java/com/socialweb/socialweb/UserControllerTest.java
+++ src/test/java/com/socialweb/socialweb/UserControllerTest.java
@@ -1,6 +1,8 @@
 package com.socialweb.socialweb;
 
 import com.socialweb.socialweb.user.User;
+import com.socialweb.socialweb.user.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,25 +12,47 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class UserControllerTest {
 
+    public static final String API_1_0_USERS = "/api/1.0/users";
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Before
+    public void cleanup() {
+        userRepository.deleteAll();
+    }
+
     @Test
     public void postUser_whenUserIsValid_receiveOk(){
+        User user = createValidUser();
+        ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+    private User createValidUser() {
         User user = new User();
         user.setUserName("testUser");
         user.setDisplayName("testDisplay");
         user.setPassword("Password");
+        return user;
+    }
 
-        ResponseEntity<Object> response = testRestTemplate.postForEntity("/api/1.0/users", user, Object.class);
+    @Test
+    public void postUser_whenUserIsValid_userSavedToDatabase(){
+        User user = createValidUser();
+        testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
 
-        assert(response.getStatusCode()).equals(HttpStatus.OK);
+        assertEquals(userRepository.count(), 1);
     }
 
 }
