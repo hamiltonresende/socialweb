1c958501cac5c7083056ac28068ee9c6a929ef03
Hamilton Resende <hamilton.rferreira@gmail.com>
2022-10-04 23:28:03 -0300

    Criando input de password repeat e botão de submit, criando seus respectivos métodos de teste.

--- react-front/src/pages/UserSignupPage.spec.js
+++ react-front/src/pages/UserSignupPage.spec.js
@@ -10,26 +10,41 @@ describe('UserSignupPage', () => {
             const { container } = render(<UserSignupPage />);
             const header = container.querySelector('h1');
             expect(header).toHaveTextContent('Sign Up');
-        })
+        });
         it('has input for display name', () => {
             const { queryByPlaceholderText } = render(<UserSignupPage />);
             const displayNameInput = queryByPlaceholderText('Your display name');
             expect(displayNameInput).toBeInTheDocument();
-        })
+        });
         it('has input for username', () => {
             const { queryByPlaceholderText } = render(<UserSignupPage />);
             const usernameInput = queryByPlaceholderText('Your username');
             expect(usernameInput).toBeInTheDocument();
-        })
+        });
         it('has input for password', () => {
             const { queryByPlaceholderText } = render(<UserSignupPage />);
             const passwordInput = queryByPlaceholderText('Your password');
             expect(passwordInput).toBeInTheDocument();
-        })
+        });
         it('has password type for password input', () => {
             const { queryByPlaceholderText } = render(<UserSignupPage />);
             const passwordInput = queryByPlaceholderText('Your password');
             expect(passwordInput.type).toBe('password');
-        })
+        });
+        it('has input for password repeat', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />);
+            const passwordRepeat = queryByPlaceholderText('Repeat your password');
+            expect(passwordRepeat).toBeInTheDocument();
+        });
+        it('has password type for password repeat input', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />);
+            const passwordRepeat = queryByPlaceholderText('Repeat your password');
+            expect(passwordRepeat.type).toBe('password');
+        });
+        it('has submit button', () => {
+            const { container } = render(<UserSignupPage />);
+            const button = container.querySelector('button');
+            expect(button).toBeInTheDocument();
+        });
     })
 })
